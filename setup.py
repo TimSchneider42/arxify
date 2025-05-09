c6bd54d42dda2702faefdc6fd7b64e2840394c64
Tim Schneider <tim@robot-learning.de>
2025-05-09 18:28:21 +0200

    Increased version number to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="arxify",
-    version="1.1.0",
+    version="1.2.0",
     description="Pack a latex project into an arxiv compatible archive.",
     long_description=long_description,
     long_description_content_type="text/markdown",
